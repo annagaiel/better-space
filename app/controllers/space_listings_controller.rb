66631c7654b8b2a934dfc6c2d93061e3fb087dbd
Anna Natorilla <annagaiel@gmail.com>
2016-05-08 15:16:51 -0700

    show all space listing and do the association

--- app/controllers/space_listings_controller.rb
+++ app/controllers/space_listings_controller.rb
@@ -2,6 +2,7 @@ class SpaceListingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @space_listings = current_user.space_listings
   end
 
   def new
--- app/models/space_listing.rb
+++ app/models/space_listing.rb
@@ -1,2 +1,3 @@
 class SpaceListing < ActiveRecord::Base
+  belongs_to :user
 end
