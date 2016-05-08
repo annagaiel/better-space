e1e485499f6d302fe0f10ce34de7c30deb459ad8
Anna Natorilla <annagaiel@gmail.com>
2016-05-08 14:40:00 -0700

    Create user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508213000) do
+ActiveRecord::Schema.define(version: 20160508213804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20160508213000) do
     t.integer  "size_height"
     t.integer  "size_length"
     t.boolean  "accessibility"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
+    t.integer  "role"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    first_name "MyString"
+  end
+
+end
