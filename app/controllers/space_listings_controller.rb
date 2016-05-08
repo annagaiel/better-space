dd8bc385e357b2240745fdb012c6b1efc4897728
Anna Natorilla <annagaiel@gmail.com>
2016-05-08 16:57:27 -0700

    Add the error messages on form partial and add show page

--- app/controllers/space_listings_controller.rb
+++ app/controllers/space_listings_controller.rb
@@ -9,10 +9,23 @@ class SpaceListingsController < ApplicationController
     @space_listings = SpaceListing.new
   end
 
-  private
-  def space_listing_params
-    params.require(:space_listing).permit(:title, :description,
-    :address, :zip_code, :city, :state,
-    :monthly_rent, :day_rent, :size_width, :size_height, :size_length)
+  def create
+    @space_listings = SpaceListing.new(space_listing_params)
+    if @space_listings.save
+      redirect_to @space_listings
+    else
+      render "new"
+    end
   end
+
+  def show
+    @space_listings = SpaceListing.find(params[:id])
+  end
+
+  private
+    def space_listing_params
+      params.require(:space_listing).permit(:title, :description,
+      :address, :zip_code, :city, :state,
+      :monthly_rent, :day_rent, :size_width, :size_height, :size_length)
+    end
 end
