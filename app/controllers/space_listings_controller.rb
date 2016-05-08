e8ed63ec3a03aaef14a5db95bdb655244402da70
Anna Natorilla <annagaiel@gmail.com>
2016-05-08 16:46:06 -0700

    Add a new form for listing

--- app/controllers/space_listings_controller.rb
+++ app/controllers/space_listings_controller.rb
@@ -6,5 +6,13 @@ class SpaceListingsController < ApplicationController
   end
 
   def new
+    @space_listings = SpaceListing.new
+  end
+
+  private
+  def space_listing_params
+    params.require(:space_listing).permit(:title, :description,
+    :address, :zip_code, :city, :state,
+    :monthly_rent, :day_rent, :size_width, :size_height, :size_length)
   end
 end
