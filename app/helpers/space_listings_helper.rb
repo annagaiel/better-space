a47a8598e413cf89f7156d061b6f811c6fbf8d38
Anna Natorilla <annagaiel@gmail.com>
2016-05-12 14:41:08 -0700

    style my kamari page info

--- app/helpers/space_listings_helper.rb
+++ app/helpers/space_listings_helper.rb
@@ -1,2 +1,10 @@
 module SpaceListingsHelper
+
+  def style_page_info(text)
+     text.gsub!(/Displaying space listings /, 'Show ')
+     text.gsub!(/<b>/, '<strong>')
+     text.gsub!(/<\/b>/, "<\/strong>")
+     text.gsub!(/in total/, 'result.')
+     text.html_safe
+  end
 end
