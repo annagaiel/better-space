24c691245f19044d59a897d91bb6b121539a0f09
Anna Natorilla <annagaiel@gmail.com>
2016-05-27 19:46:42 -0700

    add delete images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,9 @@
 class ImagesController < ApplicationController
+
+  def destroy
+    @image = Image.find(params[:id])
+    space_listing = @image.space_listing
+    @image.destroy
+    respond_to :js
+  end
 end
