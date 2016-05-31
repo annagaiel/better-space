0509f150980e7116a1dca52bc3417fee48c7a5f4
Anna Natorilla <annagaiel@gmail.com>
2016-05-30 21:52:30 -0700

    Add parse date

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,12 +6,16 @@ class BookingsController < ApplicationController
 
   def new
     @booking = Booking.new
-    @space_listing_id = params[:space_listing_id]
+    @space_listing = SpaceListing.find_by(id: params[:space_listing_id])
   end
 
   def create
-    @booking = Booking.new(booking_params)
+    @booking = Booking.new
     @booking.user_id = current_user.id
+    start_date = params[:booking][:move_in]
+    end_date = params[:booking][:move_out]
+    @booking.move_in = parse_date(start_date)
+    @booking.move_out = parse_date(end_date)
     if @booking.save
       render :show
     else
@@ -24,8 +28,15 @@ class BookingsController < ApplicationController
   end
 
   private
-    def booking_params
-      params.require(:booking).permit(:move_in, :move_out,
-        :total, :price, :space_listing_id)
-    end
+  def booking_params
+    params.require(:booking).permit(:move_in, :move_out,
+    :total, :price, :space_listing_id)
+  end
+
+  def parse_date(date)
+    month = date.split("/")[0]
+    day = date.split("/")[1]
+    year = date.split("/")[2]
+    DateTime.parse("#{day}/#{month}/#{year}")
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ActiveRecord::Base
   belongs_to :user
   belongs_to :space_listing
+
+  validates :move_in, presence: true
+  validates :move_out, presence: true
 end
