414faf2bacb5109e8b94991a3853a2a591bdd88a
Anna Natorilla <annagaiel@gmail.com>
2016-06-16 15:46:36 -0700

    Change the success message to the booking show page

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,10 +1,12 @@
 class ChargesController < ApplicationController
+  before_action :authenticate_user!
+
   def new
   end
 
   def create
     # Amount in cents
-    @amount = 5000
+    @amount = (current_booking.price * 100).to_i
 
     customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
@@ -14,11 +16,19 @@ class ChargesController < ApplicationController
     charge = Stripe::Charge.create(
     :customer    => customer.id,
     :amount      => @amount,
-    :description => 'Rails Stripe customer',
+    :description => 'Better Space Content',
     :currency    => 'usd'
     )
+
+    redirect_to booking_path(current_booking), notice:"Thanks, your purchase was successful!"
   rescue Stripe::CardError => e
     flash[:error] = e.message
-    redirect_to new_charge_path
+    redirect_to "/bookings/new/?space_listing_id=#{@current_booking.space_listing_id}"
+  end
+
+  private
+
+  def current_booking
+    @current_booking ||= Booking.find(params[:booking_id])
   end
 end
