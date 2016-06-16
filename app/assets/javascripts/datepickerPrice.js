8c6a12b5340b4c5d5f01cedeaf87508e74492138
Anna Natorilla <annagaiel@gmail.com>
2016-06-15 17:45:47 -0700

    Fix the datepicker and styled booking pages

--- app/assets/javascripts/datepickerPrice.js
+++ app/assets/javascripts/datepickerPrice.js
@@ -1,23 +1,3 @@
-$("#booking_move_in, #booking_move_out").datepicker().on('changeDate', function (e) {
-  var move_in = $('#booking_move_in').val();
-  var move_out = $('#booking_move_out').val();
-  var first = parseDate(move_in);
-  var second = parseDate(move_out);
-  var diff = daydiff(first, second);
-  var price = setPrice(diff);
-  if (price || price > 1) {
-    $('#booking_price').val(price);
-  }
-});
-
-var setPrice = function(diff){
-  var day = $('#day_rent').text();
-  var month = $('#monthly_rent').text();
-  if(diff < 31){
-    return parseInt(day) * diff
-  }
-}
-
 function parseDate(str) {
   var mdy = str.split('/');
   return new Date(mdy[2], mdy[0]-1, mdy[1]);
@@ -26,3 +6,29 @@ function parseDate(str) {
 function daydiff(first, second) {
   return Math.round((second-first)/(1000*60*60*24));
 }
+
+$("#booking_move_in").datepicker({
+    format: 'mm/dd/yyyy',
+    startDate: '+0d'
+}).on('changeDate', function(e) {
+    var tomorrow = new Date($(this).val());
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    $('#booking_move_out').datepicker('setStartDate', tomorrow);
+    $('#booking_move_out').datepicker('update');
+});
+
+$('#booking_move_in').datepicker('setDate', new Date());
+
+$("#booking_move_in, #booking_move_out").datepicker().on('changeDate', function (e) {
+  var move_in = $('#booking_move_in').val();
+  var move_out = $('#booking_move_out').val();
+  var first = parseDate(move_in);
+  var second = parseDate(move_out);
+  var diff = daydiff(first, second);
+  var day = $('#day_rent').val();
+  var total =  parseInt(day) * diff;
+  console.log(parseInt(day) * diff);
+  if (move_in !== "" && move_out !== ""){
+    $('#booking_price').val(total);
+  }
+});
