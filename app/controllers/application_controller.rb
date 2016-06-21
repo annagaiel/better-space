44cb88633616d4f1aca6d24f2429a6516414aef4
Anna Natorilla <annagaiel@gmail.com>
2016-06-21 15:46:00 -0700

    fix footer error and refactor space listing map and list view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_footer
 
   protected
 
@@ -10,4 +11,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :first_name, :last_name, :phone_number) }
   end
 
+  private
+  def set_footer
+    @recent_listings = SpaceListing.order("created_at DESC").limit(4)
+    @top_hosts = User.order(:created_at).limit(4)
+    @garage_count = SpaceListing.where("space_type": "Garage").count
+    @house_count = SpaceListing.where("space_type": "House").count
+    @outdoor_count = SpaceListing.where("environment_type": "Outdoor").count
+    @indoor_count = SpaceListing.where("environment_type": "Indoor").count
+  end
 end
--- app/controllers/space_listings_controller.rb
+++ app/controllers/space_listings_controller.rb
@@ -3,29 +3,7 @@ class SpaceListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
 
   def index
-    page_limit = params[:page] || 1
-
-    if current_user
-      @space_listings = current_user.space_listings.page(params[:page]).per(3)
-    else
-      @space_listings = SpaceListing.all.page(params[:page]).per(3)
-    end
-    @recent_listings = SpaceListing.order("created_at DESC").limit(4)
-    @top_hosts = User.order(:created_at).limit(4)
-    @garage_count = SpaceListing.where("space_type": "Garage").count
-    @house_count = SpaceListing.where("space_type": "House").count
-    @outdoor_count = SpaceListing.where("environment_type": "Outdoor").count
-    @indoor_count = SpaceListing.where("environment_type": "Indoor").count
-  end
-
-  def list_view
     @space_listings = SpaceListing.all
-    @recent_listings = SpaceListing.order("created_at DESC").limit(4)
-    @top_hosts = User.order(:created_at).limit(4)
-    @garage_count = SpaceListing.where("space_type": "Garage").count
-    @house_count = SpaceListing.where("space_type": "House").count
-    @outdoor_count = SpaceListing.where("environment_type": "Outdoor").count
-    @indoor_count = SpaceListing.where("environment_type": "Indoor").count
   end
 
   def new
